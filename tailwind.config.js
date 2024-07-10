196a7cf77c693e31527438df86eeeaeaecfe8a9b
edanurmutlu <112180102+edamuutlu@users.noreply.github.com>
2024-07-10 17:25:14 +0300

    Project Setup

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+const { extend } = require('dayjs');
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  purge: {
+    purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
+    darkMode: false,
+    theme: {
+      extend: {},
+    },
+    variants: {
+      extend: {},
+    },
+    plugins: [require('@tailwindcss/forms')],
+  },
+}
+
